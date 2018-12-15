a813d1dbb7857e5db234b0e855164bb33c1df19f
grgsiberia <nanashi4129@gmail.com>
2018-12-15 14:43:56 +0900

    プロジェクトの立ち上げ

---
+++ STAILIST-FORTRAN/STAILIST-FORTRAN/STAILIST-FORTRAN.f90
@@ -0,0 +1,25 @@
+!  STAILIST_FORTRAN.f90 
+!
+!  関数:
+!  STAILIST_FORTRAN - コンソール・アプリケーションのエントリーポイント。
+!
+
+!****************************************************************************
+!
+!  プログラム: STAILIST_FORTRAN
+!
+!  目的:  コンソール・アプリケーションのエントリーポイント。
+!
+!****************************************************************************
+
+    program STAILIST_FORTRAN
+
+    implicit none
+
+    ! 変数
+
+    ! STAILIST_FORTRAN の本文
+    print *, 'Hello World'
+
+    end program STAILIST_FORTRAN
+
